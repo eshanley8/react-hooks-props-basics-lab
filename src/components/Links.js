8e7b47768e1e50114987bb49d05cb145af853c10
eshanley8 <91692882+eshanley8@users.noreply.github.com>
2023-06-06 15:00:56 -0700

    complete

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+import user from "../data/user";
+
+console.log(user)
+const Links = (props) => {
+    const {github, linkedin} = props
+    return <div>
+    <h3>Links</h3>
+    <a href= {github}>{github} </a>
+     <a href= {linkedin}>{linkedin}</a>
+    </div>
+}
+export default Links
